bb48eb7d0ab332bb16c7529cb18974e8f4bca9fe
root <root@DESKTOP-482253N.localdomain>
2021-09-21 20:57:31 +1000

    assignment passed

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,1 +1,33 @@
+class Doctor
 
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    #Appointment gets initialized with date, patient, and doctor (in that order)
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def appointments
+        Appointment.all.select do |app|
+            app.doctor == self
+        end
+    end
+
+    def patients
+        appointments.map do |app|
+            app.patient    
+        end
+    end
+
+end
--- lib/patient.rb
+++ lib/patient.rb
@@ -1,1 +1,37 @@
+class Patient
 
+    @@all = []
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    #Appointment gets initialized with date, patient, and doctor (in that order)
+    def new_appointment(date, doctor)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select do |app|
+            app.patient == self
+        end
+    end
+
+    def doctors
+        appointments.map do |app|
+            app.doctor   
+        end
+    end
+
+
+
+
+
+end
